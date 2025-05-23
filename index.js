67d04b1779a4b2ec223f0263f18827c9ead0a6cf
fernanda-vaz <fernandavazdev@gmail.com>
2025-05-22 23:41:08 -0300

    revisao do codigo

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
 let herois = [
   ['Arthas', 7321],
-  ['Zafira', 14322],
-  ['Thalor', 12897],
+  ['Zafira', 9322],
+  ['Thalor', 197],
   ['Morgana', 5412],
   ['Draven', 8900],
-  ['Selene', 15000],
+  ['Selene', 7042],
   ['Kael', 4789],
-  ['Lyanna', 13345],
+  ['Lyanna', 1045],
   ['Darius', 6210],
   ['Freya', 11567],
 ]
 
 for (let i = 0; i < herois.length; i++) {
+  let nivelHeroi
+
   if (herois[i][1] <= 1000) {
-    let nivelHeroi = 'Ferro'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Ferro'
   } else if (herois[i][1] <= 2000) {
-    let nivelHeroi = 'Bronze'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Bronze'
   } else if (herois[i][1] <= 5000) {
-    let nivelHeroi = 'Prata'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Prata'
   } else if (herois[i][1] <= 7000) {
-    let nivelHeroi = 'Ouro'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Ouro'
   } else if (herois[i][1] <= 8000) {
-    let nivelHeroi = 'Platina'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Platina'
   } else if (herois[i][1] <= 9000) {
-    let nivelHeroi = 'Ascendente'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Ascendente'
   } else if (herois[i][1] <= 10000) {
-    let nivelHeroi = 'Imortal'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Imortal'
   } else {
-    let nivelHeroi = 'Radiante'
-    console.log(
-      'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
-    )
+    nivelHeroi = 'Radiante'
   }
+  console.log(
+    'O Herói de nome ' + herois[i][0] + ' está no nível ' + nivelHeroi
+  )
 }
